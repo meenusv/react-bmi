0b0996ea7c4097a51e37b66f57c64f18a1237f82
meenusv <meenuvishwa1920@gmail.com>
2020-07-20 08:56:17 +0530

    Updates

---
+++ precache-manifest.2bfbd39c2216d8761146d37370edd569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eeadb567741adb1cf1b54315b94d491",
+    "url": "/react-bmi/index.html"
+  },
+  {
+    "revision": "93e2f4cdb458f9d136d9",
+    "url": "/react-bmi/static/css/main.f91e32c4.chunk.css"
+  },
+  {
+    "revision": "71a781b61d5554e69f5a",
+    "url": "/react-bmi/static/js/2.9411a8ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bmi/static/js/2.9411a8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e2f4cdb458f9d136d9",
+    "url": "/react-bmi/static/js/main.fb66b70d.chunk.js"
+  },
+  {
+    "revision": "29e06b274ba042c1c1ce",
+    "url": "/react-bmi/static/js/runtime-main.6a29a6b5.js"
+  }
+]);
